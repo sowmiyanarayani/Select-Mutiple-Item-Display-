db5dd0a2857286d617cbf330340fbd42b2448e07
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-02 20:39:50 +0530

    Config Commit

--- src/core/config.js
+++ src/core/config.js
@@ -1,3 +1,17 @@
-const config = {};
+const config = {
+	idLength: 8,
+	products: [
+		{ name: 'Milk', price: 10 },
+		{ name: 'Biscuit', price: 15 },
+		{ name: 'Chocolate', price: 20 },
+		{ name: 'Butter', price: 150 },
+		{ name: 'Maggie', price: 30 },
+		{ name: 'Dry Fruits', price: 550 },
+		{ name: 'Cheese', price: 200 },
+		{ name: 'Refined Oil', price: 650 },
+		{ name: 'Face Wash', price: 70 },
+		{ name: 'Soap', price: 45 },
+	],
+};
 
 export default config;
