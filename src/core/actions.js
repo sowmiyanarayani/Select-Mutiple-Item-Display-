b45eb1185a59d3848c1c76bd0ea02087c7b992a6
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-02 20:49:05 +0530

    Action Commit

--- src/core/actions.js
+++ src/core/actions.js
@@ -1,3 +1,7 @@
-const actions = {};
+import CustomerManager from '../services/CustomerManager';
+const setSelectItems = ({ data }) => ({ items: data });
+const addItems = (context) =>
+	({ itemDetails: CustomerManager.addItems(context) });
+const actions = { setSelectItems, addItems };
 
 export default actions;
