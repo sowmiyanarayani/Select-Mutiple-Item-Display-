58dcf0a37f387c3bd1992fd975b22352585995b6
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-02 20:45:56 +0530

    Table Commit

---
+++ src/components/TableForm.js
@@ -0,0 +1,27 @@
+/* eslint-disable react/react-in-jsx-scope */
+/* eslint-disable max-lines-per-function */
+import Customer from './Customer';
+const CustomerList = (context) => {
+	const { state: { itemDetails }} = context;
+
+	return (
+		<table className="gird">
+			<thead>
+				<tr>
+					<th>item</th>
+					<th>price</th>
+				</tr>
+			</thead>
+			<tbody>
+				{ itemDetails.map((customer) =>
+					<Customer
+						key={ customer.id }
+						items={ customer.items }
+						price={ customer.price }
+					/>) }
+			</tbody>
+		</table>
+	);
+};
+
+export default CustomerList;
