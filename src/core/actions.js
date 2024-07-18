cf5802539d035145540edb6ba5a1cdcf42b400e8
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-07-18 12:13:00 +0530

    Context Commit

--- src/core/actions.js
+++ src/core/actions.js
@@ -2,7 +2,7 @@ import CustomerManager from '../services/CustomerManager';
 const setSelectItems = ({ data }) => ({ items: data });
 const addItems = (context) =>
 	({ itemDetails: CustomerManager.addItems(context) });
-const setTotalPrice = ({ data }) => ({ totalPrice: data });
-const actions = { setSelectItems, addItems, setTotalPrice };
+
+const actions = { setSelectItems, addItems };
 
 export default actions;
--- src/core/seed.js
+++ src/core/seed.js
@@ -1,7 +1,7 @@
 const seed = {
 	items: [],
 	itemDetails: [],
-	totalPrice: [],
+
 };
 
 export default seed;
